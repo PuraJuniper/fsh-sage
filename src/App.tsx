8d43954f304f00d1442b40b40bc043e26f9f6c96
Tony Song <tonysong@Tonys-MacBook-Pro.local>
2021-11-25 10:03:19 -0500

    added common metadata

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {SelectView} from "./selectView"
 import {Collection} from "./collection"
+import {MetaData} from "./metaData"
 
 function App() {
   return (
     <div className="App container">
-      <SelectView />
-      <Collection />
+      <MetaData/>
+      {/* <SelectView />
+      <Collection /> */}
     </div>
   );
 }
