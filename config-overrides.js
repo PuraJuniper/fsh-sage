86404a1391779da1e84b92a8b6dc7c0c5df78a3a
Raghavan Chandrabalan <raghavan.chandrabalan@purajuniper.com>
2021-11-29 12:30:22 -0500

    add override for node functions

---
+++ config-overrides.js
@@ -0,0 +1,31 @@
+module.exports = function override(config, env) {
+    // config is webpack config
+    config.module.rules.push(
+      {
+        test: /.*fs-extra.*/,
+        use: 'null-loader'
+      },
+      {
+        test: /.*fs-minipass.*/,
+        use: 'null-loader'
+      },
+      {
+        test: /.*readline-sync.*/,
+        use: 'null-loader'
+      },
+      {
+        test: /.*tar(\\|\/).*/,
+        use: 'null-loader'
+      },
+      {
+        test: /.*uglify.*/,
+        use: 'null-loader'
+      },
+      {
+        test: /.*gofsh.*.d\.ts$/,
+        loader: 'null-loader'
+      }
+    );
+    return config;
+  };
+  
