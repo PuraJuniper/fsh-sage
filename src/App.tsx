04f032e23fd9b9cd665ed4b469c8b7e3fb729bdc
Winston Leung <winstonleung@Winstons-MacBook-Pro.local>
2021-11-29 12:02:15 -0500

    Checking bug for running sushi

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,47 @@ import {Collection} from "./collection"
 import {MetaData} from "./metaData"
 import {BaseCard} from "./baseCard"
 import { CSSTransition } from 'react-transition-group';
+import {runSUSHI} from "./utils/FSHHelpers"
 
 function App() {
+  const rs = async () => {
+  const outPackage = await runSUSHI(`
+    // @Name: Simple Extensions
+    // @Description: Examples of extensions with values (no sub-extensions)
+
+    Extension: Laterality
+    Description: "Body side of a body location."
+    * value[x] only CodeableConcept
+    * value[x] from http://hl7.org/fhir/ValueSet/bodysite-laterality (required)
+
+    Extension: TherapySessionsCompleted
+    Id:        therapy-sessions-completed
+    Title:     "Therapy Sessions Completed"
+    Description: "The number of sessions of some therapy."
+    // Limit the context to Procedures -- Also see Rule Sets for a context-setting rule set
+    * ^context[+].type = #element
+    * ^context[=].expression = "Procedure"
+    * value[x] only unsignedInt  // 0 or more
+
+    Extension: RelatedCondition
+    Id:        related-condition
+    Title:    "Condition related to the current resource"
+    Description: "The resource has an unspecified relationship with a Condition."
+    * value[x] only Reference(Condition)
+    `);
+    }
+  rs();
+
+  return <div></div>;
+
+
+
+
+
+
+
+
+
   let [render, setRender] = useState("metadata");
   let [nextRender, setNextRender] = useState("");
   let [show, setShow] = useState(true);
---
+++ src/utils/Load.js
@@ -0,0 +1,92 @@
+import tarStream from 'tar-stream';
+import zlib from 'zlib';
+import http from 'http';
+import { utils } from 'fsh-sushi';
+const logger = utils.logger;
+
+export function unzipDependencies(resources, dependency, id) {
+  let returnPackage = { resourceArr: resources, emptyDependencies: [] };
+  return new Promise((resolve) => {
+    http.get(`https://packages.fhir.org/${dependency}/${id}`, function (res) {
+      const extract = tarStream.extract();
+      // Unzip files
+      extract.on('entry', function (header, stream, next) {
+        let buf = '';
+        stream.on('data', function (chunk) {
+          buf += chunk.toString();
+        });
+        stream.on('end', function () {
+          try {
+            const resource = JSON.parse(buf);
+            if (resource.resourceType) {
+              returnPackage.resourceArr.push(resource);
+            }
+          } catch {} //eslint-disable-line no-empty
+          next();
+        });
+        stream.resume();
+      });
+      extract.on('finish', function () {
+        resolve(returnPackage);
+      });
+      if (res.statusCode < 400) {
+        res.pipe(zlib.createGunzip()).pipe(extract);
+        logger.info(`Downloaded ${dependency}#${id}`);
+      } else {
+        if (id === 'current' || id === 'dev') {
+          logger.error(`FSHOnline does not currently support "current" or "dev" package versions`);
+        } else {
+          logger.error(`your dependency ${dependency}#${id} could not be loaded. Your output may be invalid.`);
+          returnPackage.emptyDependencies.push(`${dependency}${id}`);
+        }
+        resolve(returnPackage);
+      }
+    });
+  });
+}
+
+export function loadDependenciesInStorage(database, resources, dependency, id) {
+  return new Promise((resolve, reject) => {
+    // Loads parsed json into indexDB
+    const transaction = database.transaction([`${dependency}${id}`], 'readwrite');
+    transaction.oncomplete = () => {
+      resolve();
+    };
+    transaction.onerror = (event) => {
+      reject(event);
+    };
+    const objectStore = transaction.objectStore(`${dependency}${id}`, { keyPath: ['id', 'resourceType'] });
+    resources.forEach((res) => {
+      if (res.id && res.resourceType) {
+        objectStore.put(res);
+      }
+    });
+  });
+}
+
+export function loadAsFHIRDefs(FHIRdefs, database, dependency, id) {
+  // Convert database data into FHIR Definitions
+  return new Promise((resolve, reject) => {
+    let displayLoaded = false;
+    const getData = database
+      .transaction([`${dependency}${id}`], 'readonly')
+      .objectStore(`${dependency}${id}`, { keyPath: ['id', 'resourceType'] })
+      .openCursor();
+    getData.onerror = function () {
+      reject('There is an error getting data out!');
+    };
+    getData.onsuccess = function () {
+      const iterator = getData.result;
+      if (iterator) {
+        displayLoaded = true;
+        FHIRdefs.add(iterator.value);
+        iterator.continue();
+      } else {
+        if (displayLoaded) {
+          logger.info(`Loaded package ${dependency}#${id}`);
+        }
+        resolve(FHIRdefs);
+      }
+    };
+  });
+}
